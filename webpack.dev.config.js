99a055eec8dab5e74211b89e5326bc10f150555e
qiancheng.zhao <452-qiancheng.zhao@users.noreply.git.saybot.net>
2023-07-06 13:08:49 +0800

    pre

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,12 +1,20 @@
 const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     mode: "development",
     entry: path.join(__dirname, './src/main.ts'),
     output: {
         path: path.join(__dirname, './dist'),
-        filename: 'bundle.js',
+        filename: '[name].js',
 
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+    ],
     resolve: {
         extensions: ['.js', '.ts',],
     },
